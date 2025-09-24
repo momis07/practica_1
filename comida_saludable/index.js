3cc531acaacee7514c5954982620f512ac5ec05c
Belen Rosales <belen.08.abril@gmail.com>
2025-09-23 20:51:23 -0600

    first commit

---
+++ comida_saludable/index.js
@@ -0,0 +1,29 @@
+let pantalla= document.querySelector('.form-control');
+let botones= document.querySelectorAll('button');
+
+function limpiar(){
+    pantalla.value='';
+}
+function calcular (){
+    try{
+        pantalla.value=eval(pantalla.value);
+    }catch{
+        pantalla.value='Error';
+        pantalla.value += texto +"+";
+    }
+}
+function escribir (texto){
+pantalla.value += texto +"+";
+}
+botones.forEach(boton=> {
+    boton.addEventListener('click',() =>{
+        let texto =boton.textContent;
+        if(texto ==='Limpiar'){
+            limpiar()
+        }else if(texto ==='='){
+          calcular();
+            }else{
+                escribir(texto);
+         }
+});
+});
